a3a0f06a00cacbb2edec03eb5566c42532099474
GeronimoSmeebl <83225337+GeronimoSmeebl@users.noreply.github.com>
2021-04-26 13:47:30 -0400

    Add files via upload

--- mining_v2.py
+++ mining_v2.py
@@ -17,10 +17,7 @@ Currently_Mining = False
 # http://razorenhanced.net/dokuwiki/doku.php?id=resourcegatheringscripts
 
 #
-import g_tools
-
 import clr, time, thread, sys, System
-
 clr.AddReference('System')
 clr.AddReference('System.Drawing')
 clr.AddReference('System.Windows.Forms')
@@ -36,18 +33,115 @@ from System.Windows.Forms import (Application, Button, Form, BorderStyle, Label,
  DataGridViewAutoSizeColumnsMode, DataGridViewSelectionMode, DataGridViewEditMode, RadioButton, GroupBox,
  TextBox, CheckBox, ProgressBar)
 from System.ComponentModel import BackgroundWorker
-
 #
 
 # Values from Credzba and Fate's Whim's mining scripts
 shovelID = 0x0F39
 pickaxeID = 0x0E86
+tinker_kitsID = 0x1EB8
+metalID = 0x1BF2
 waitForTarget = 5000
 waitAfterMining = 2500
 waitAfterPickAxe = 4000
 pickaxe_spots = List[int] (( 0x136D, 0x1367, 0x136A)) 
 CaveTiles = [ 0x0016, 0x0017, 0x0018, 0x0019, 0x245, 0x246, 0x247, 0x248, 0x249, 0x22b, 0x22c, 0x22d, 0x22e, 0x22f ]
 SoilTiles = [ 0x0073, 0x0074, 0x0075, 0x0076,  0x0077, 0x0078 ]
+#
+
+
+# Based on function from Credzba's common.py
+def find_items(item_types, item_serial=Player.Backpack.Serial):
+    found_items = []    
+    cur_item = Items.FindBySerial(item_serial)
+    
+    # If the serial does not correspond to an item, no items can be found
+    if not cur_item:
+        return found_items
+
+    # If I was looking for this item, add this item to the list of found items
+    if cur_item.ItemID in item_types:
+        found_items.append(cur_item)
+
+    # If the item is not a container, no need to search within item for contained items
+    if not cur_item.IsContainer:  
+        return found_items
+
+    # Otherwise, search inside of item for contained items
+    
+    # If current item is an empty container, it does not contain any items
+    if len(cur_item.Contains) == 0:
+        return found_items
+    
+    
+
+    # Otherwise, look at all contained items and their potential contents with this same function for the same item types
+    for item in cur_item.Contains:
+        for contained_in_item in find_items(item_types, item.Serial):
+            found_items.append(contained_in_item)
+    return found_items
+#
+
+# Based on function from Credzba's common.py
+def make_tinker():
+    kits_in_pack = find_items([tinker_kitsID])
+    
+    # If no tinker kits found in backpack, exit with message
+    if len(kits_in_pack) == 0:
+        Misc.SendMessage("Need tinker kit to craft a tinker kit.")
+        sys.exit()
+    #
+    
+    # Arbitrarily use the first kit found in pack
+    use_kit = kits_in_pack[0]
+
+    # Make sure user has enough metal to craft tinker kit
+    if Items.BackpackCount(metalID, 0) < 2:
+        Misc.SendMessage("Not enough iron to craft tinker kit.")
+        sys.exit()
+    #
+    
+    # Craft tinkering kit via tinkering GUMP
+    Items.UseItem(use_kit)
+    Gumps.WaitForGump(949095101, 2000)
+    Gumps.SendAction(949095101, 15)
+    Gumps.WaitForGump(949095101, 2000)
+    Gumps.SendAction(949095101, 23)
+    Gumps.WaitForGump(949095101, 2000)
+    Gumps.SendAction(949095101, 0)
+    Gumps.WaitForGump(949095101, 2000) 
+    Target.Cancel()
+#
+
+# Based on function from Credzba's common.py
+def make_shovel():    
+    kits_in_pack = find_items([tinker_kitsID])
+    
+    # If no tinker kits found in backpack, exit with message
+    if len(kits_in_pack) == 0:
+        Misc.SendMessage("Need tinker kit to craft shovel.")
+        sys.exit()
+    #
+    
+    # Arbitrarily use the first kit found in pack
+    use_kit = kits_in_pack[0]
+    
+    # Make sure user has enough metal to craft shovel
+    if Items.BackpackCount(metalID, 0) < 4:
+        Misc.SendMessage("Not enough iron to craft shovel.")
+        sys.exit()
+    #
+
+    # Craft shovel via tinkering GUMP
+    Items.UseItem(use_kit)
+    Gumps.WaitForGump(949095101, 2000)
+    Gumps.SendAction(949095101, 15)
+    Gumps.WaitForGump(949095101, 2000)
+    Gumps.SendAction(949095101, 72)
+    Gumps.WaitForGump(949095101, 2000)
+    Gumps.SendAction(949095101, 0)    
+    Gumps.WaitForGump(949095101, 2000) 
+    Target.Cancel()
+#
 #
 
 # Based on function from Credzba's mining.py
@@ -214,17 +308,17 @@ quit_flag = False
 def make_tool():
     global quit_flag
 
-    if Items.BackpackCount( g_tools.tinker_kitsID, 0 ) < 2:
+    if Items.BackpackCount( tinker_kitsID, 0 ) < 2:
         if quit_flag:
             Player.HeadMessage(10, "Unable to make tool(s)")
             sys.exit()
         else:
-            g_tools.make_tinker()
+            make_tinker()
             quit_flag = true
             make_tool()
         #
     else:
-        g_tools.make_shovel()
+        make_shovel()
         quit_flag = False
 #
 
@@ -234,7 +328,7 @@ def get_tool():
     if Manual_Tool != None:
         return Manual_Tool
 
-    tool_list = g_tools.find_items([pickaxeID, shovelID], Player.Backpack.Serial)
+    tool_list = find_items([pickaxeID, shovelID], Player.Backpack.Serial)
 
     for item in tool_list:
         if item.ItemID == pickaxeID and ("garg" in item.Name.lower()) == using_garg:
